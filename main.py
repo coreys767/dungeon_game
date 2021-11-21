6d72fdf73f6ff6c8be7351515147498adee49cfd
coreys767 <88414017+coreys767@users.noreply.github.com>
2021-11-20 22:45:37 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+
+def main():
+
+    print("Out on the open seas, a spooky ship pulls aside your own. You hide, and see a figure come onto your ship."
+          "It appears to be a pirate, and he's glowing... like a ghost. He goes through your ship as you continue"
+          "to hide, then returns to his own ship. the two are still connected and you feel compelled to jump on his"
+          "ship to look around, cause seriously why did that ghost need a roast beef sandwich? Upon getting on the"
+          " ship you notice a map on the ship and take it. Time to explore, but stay away from the captains chamber"
+          "till the end. That must be where the ghost is.")
+# variables
+#   dictionaries: the rooms( *deck, *captains chamber, 1crows nest, 2mess hall, 3armory, 4bunks, 5study, 6infirmary,
+#       7hallway, 8prison) contain connecting rooms with value door and an item until its popped
+#
+#   list: current inventory
+#   string: current room
+#
+#   create while loop for actions
+#   possible actions: help(tells actions), map( tells what rooms there are and which ones you have been to ),
+#   inventory(shows what items you have)
+#   loop exits when you enter the captains chamber and goes to story mode
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
